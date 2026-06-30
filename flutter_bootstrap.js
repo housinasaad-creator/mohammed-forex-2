275affb60c6fb0283576c73000fd3db186f07547
Mohammed <housin.asaad@gmail.com>
2026-07-01 02:19:49 +0300

    Fix base-href to /mohammed-forex-2/ for GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2666461504" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3391430520" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
