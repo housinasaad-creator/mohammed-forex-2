93e10e295bca72a48ecfb35cb2b40bfdba2e33a2
Mohammed <housin.asaad@gmail.com>
2026-07-01 14:34:03 +0300

    feat: neon hover glow on asset rows in sidebar

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2541823277" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3881508998" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
