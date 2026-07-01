a4aef5cfb5bb3131a9952f6ded2ff57d148863d4
Mohammed <housin.asaad@gmail.com>
2026-07-01 14:30:47 +0300

    feat: neon colors always visible + hover glow on category headers

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3879482976" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2541823277" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
