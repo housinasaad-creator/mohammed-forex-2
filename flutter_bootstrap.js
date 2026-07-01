bb84cae54a79d6be0712313bcdc5e8714e014318
Mohammed <housin.asaad@gmail.com>
2026-07-01 11:09:36 +0300

    feat: session overlap detection + glowing badge UI + PNG pill

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2243910713" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3303041210" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
