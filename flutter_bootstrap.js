694a8e64be3a498277820ddfce2fb4b570cae73a
Mohammed <housin.asaad@gmail.com>
2026-07-01 04:17:37 +0300

    polish: localize empty states, improve idle chart placeholder, decision badge placeholder

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "817917710" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2243910713" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
