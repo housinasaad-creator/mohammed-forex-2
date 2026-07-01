851fdeca1eb027c3d3771ac9cb4581499f00cb24
Mohammed <housin.asaad@gmail.com>
2026-07-01 03:07:31 +0300

    feat: candle colors match badges, navy bg, remove watermark, 1400x720 resolution

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "981159576" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1639287919" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
