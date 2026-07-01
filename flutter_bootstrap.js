0756677e9a1548824eb503afcdd4ac0af5c641a0
Mohammed <housin.asaad@gmail.com>
2026-07-01 03:56:44 +0300

    fix: strategy strip scrollable so all 5 cells always visible

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2200331218" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1924335332" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
