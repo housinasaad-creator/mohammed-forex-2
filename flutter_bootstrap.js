c6a317533161e0dd9c61e16acef5718d093d4bc8
Mohammed <housin.asaad@gmail.com>
2026-07-01 12:30:58 +0300

    fix: remove strategy strip from PNG, keep only in browser UI inline header

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "307140609" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1173176664" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
